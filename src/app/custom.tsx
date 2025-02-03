9de0a6fed8524b53c7591a8589baaca881548315
Klaus Chamberlain <masterlink.robot@gmail.com>
2025-02-02 22:05:29 -0500

    fixed deployment issues

--- src/app/custom.tsx
+++ src/app/custom.tsx
@@ -4,25 +4,21 @@ import logo from "./logo.png";
 function GameInfo() {
   return (
     <div className="game-info-container">
-      <img src={logo.src} alt="Logo" className="game-info-img" />
+      <Image src={logo} alt="Logo" className="game-info-img" />
       <h1 className="game-info-header">Welcome to the War of the Elector!</h1>
       <p className="game-info-description">
         {" "}
         This is a web app I (klaus) am making for Noah's new game. Functionality
         is limited, but I will add more as we go along. Current functionalities
         in development for release in first issue are:
-        <ul>
-          <li>
-            <strong>User Authentication:</strong> Implement user functionality
-          </li>
-          <li>
-            <strong>Resource Management:</strong> View and manage your
-            resources/money (send and recieve)
-          </li>
-        </ul>
-        <ul>
-          <li>More features will be added as we go along.</li>
-        </ul>
+        <li>
+          <strong>User Authentication:</strong> Implement user functionality
+        </li>
+        <li>
+          <strong>Resource Management:</strong> View and manage your
+          resources/money (send and recieve)
+        </li>
+        More features will be added as we go along.
       </p>
     </div>
   );
