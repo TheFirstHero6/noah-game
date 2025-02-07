91221edbf9deadc76b0b7623d5e5b544fe98268b
Klaus Chamberlain <masterbenedict.robot@gmail.com>
2025-02-06 22:44:02 -0500

    simple temp database solution not utilizing endpoint will work on that in dev for release on v2

--- prisma/migrations/20250207032729_database_attempt/migration.sql
+++ prisma/migrations/20250207032729_database_attempt/migration.sql
@@ -4,9 +4,10 @@ CREATE TYPE "Role" AS ENUM ('BASIC', 'ADMIN');
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "clerkID" TEXT NOT NULL,
+    "name" TEXT,
+    "imageUrl" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'BASIC',
@@ -15,7 +16,7 @@ CREATE TABLE "User" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_clerkID_key" ON "User"("clerkID");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
