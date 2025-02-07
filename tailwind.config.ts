d110bbdf461c95930340909d33ea1f1d0a2537d8
Klaus Chamberlain <masterbenedict.robot@gmail.com>
2025-02-06 20:02:41 -0500

    fixed placeholder styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,22 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "sword-unsheath": "unsheath 1s ease-in-out forwards",
+      },
+      keyframes: {
+        unsheath: {
+          "0%": {
+            transform: "scaleX(0)",
+          },
+          "50%": {
+            transform: "scaleX()1.2",
+          },
+          "100%": {
+            transform: "scaleX(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
