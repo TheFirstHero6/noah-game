7325503a182855e3247bf130d9fa4771387cc4c9
Klaus Chamberlain <masterbenedict.robot@gmail.com>
2025-02-07 02:47:35 -0500

    attempting deployment after successful clerk/db sync test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import GameInfo from "./custom";
-import prisma from "../lib/db";
+import prisma from "./lib/db";
+import { Prisma } from "@prisma/client";
 import { currentUser } from "@clerk/nextjs/server";
 export default async function Home() {
   const user = await currentUser();
   if (!user) {
     return null;
   }
+  console.log("Connecting to:", process.env.DATABASE_URL);
 
   const loggedInUser = await prisma.user.findUnique({
     where: { clerkUserId: user.id },
   });
-
   if (!loggedInUser) {
     await prisma.user.create({
       data: {
@@ -21,7 +22,6 @@ export default async function Home() {
       },
     });
   }
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
