405780fc24ed2956b954f18cb549456e5cbd1901
Klaus Chamberlain <masterbenedict.robot@gmail.com>
2025-02-24 20:53:13 -0600

    still having userpic issue, gotta get to the bottom of that. fixed dashboard not protected route(since I recently put the dashboard inside of pages), and centered sign-up ui component

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <div className="sign-in-container">
+      <SignUp />
+    </div>
+  );
 }
